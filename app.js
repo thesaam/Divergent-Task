2718409e0860a91abdb256b1e680553697a41ba0
saamlegend <bakareayomiku@gmail.com>
2021-05-24 05:04:12 +0100

    Task-done

---
+++ app.js
@@ -0,0 +1,33 @@
+function button() {
+  var send = 'LOG IN';
+  var info = 'Please Click OK to display input value';
+  var final = (send + '\n' + info)
+  alert (final)
+
+  var emailInput = document.getElementById("email").value;
+  var clickOk = '\nClick OK for hte next output';
+  alert('Email: ' + emailInput + clickOk);
+
+  var pwd = document.getElementById("password").value;
+  alert('Password: ' + pwd);
+}
+
+function Register() {
+  alert ("Don't have an account? Register")
+}
+
+function forgot() {
+  alert ("Forgot Password")
+}
+
+function submit() {
+
+}
+
+function getInputValue(){
+  // Selecting the input element and get its value 
+  var emailInput = document.getElementById("email").value;
+  
+  // Displaying the value
+  alert(emailInput);
+}
